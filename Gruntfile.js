8818a8fdafbbb56c33b4da401ac03d48b4c15cc9
Ryan Parsley <ryan.parsley@rockfishinteractive.com>
2015-05-28 16:27:24 -0400

    add grunt tasks

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function(grunt) {
+  'use strict';
+  require('load-grunt-tasks')(grunt);
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    watch: {
+      scripts: {
+        files: ['copy/**/*.html'],
+        tasks: ['hbs'],
+        options: {
+          spawn: false,
+          atBegin: true
+        },
+      }
+    },
+    shell: {
+      options: {
+        stderr: false
+      },
+      parse: {
+        command: 'node indesignToHTML.js'
+      }
+    },
+    hbs:
+      {
+      all:
+        {
+        options:
+          {
+          layout: 'src/default.hbs',
+          partials:
+            {
+            'header': 'src/partials/header.html',
+            'aside': 'copy/fragments/about-sidebar.html'
+          }
+        },
+        files:
+          {
+          'index.html': {
+            source: 'copy/fragments/index.html',
+            context: { aside: false }
+          },
+          'about.html':
+            {
+            source: 'copy/fragments/about.html',
+            context: { aside: true }
+          },
+          'pricing.html':
+            {
+            source: 'copy/fragments/pricing.html',
+            context: { aside: true }
+          }
+        }
+      }
+    }
+  });
+
+  // Default task(s).
+  grunt.registerTask('default', ['shell:parse', 'hbs']);
+
+};
